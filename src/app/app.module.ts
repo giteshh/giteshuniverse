c829ff2c532631fd3b641f1fd9d39e38527c8402
Gitesh Sharma <gitesh15796@gmail.com>
2023-03-14 12:16:50 +0530

    app files modifications done from old project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {BackgroundComponent} from './pages/background/background.component';
+import {SkillsComponent} from './pages/skills/skills.component';
+import {NavbarComponent} from './pages/navbar/navbar.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    BackgroundComponent,
+    SkillsComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AppRoutingModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
